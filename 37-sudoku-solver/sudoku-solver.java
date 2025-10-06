ed0db241d3f7ea968c402575e9eb4e31d8c1ef5d
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2025-10-07 02:58:23 +0530

    Time: 323 ms (40.43%) | Memory: 41.6 MB (17.88%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -24,18 +24,16 @@ class Solution {
             }
         }
     }
-    private boolean isValid(char[][] board, int row, int col, char num) {
-        for (int j = 0; j < 9; j++) {
-            if (board[row][j] == num) return false;
-        }
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == num) return false;
-        }
-        int sRow = (row / 3) * 3;
-        int sCol = (col / 3) * 3;
-        for (int i = sRow; i < sRow + 3; i++) {
-            for (int j = sCol; j < sCol + 3; j++) {
-                if (board[i][j] == num) return false;
+    public boolean isValid(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == c){
+                return false; // column check
+            }
+            if(board[row][i] == c){ // row check
+                return false;
+            }
+            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c){ // 3x3 grid check
+                return false;
             }
         }
         return true;
