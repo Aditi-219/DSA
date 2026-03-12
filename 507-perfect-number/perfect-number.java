6d71fc354e11ec90ed9b7215c7b2e164c3a27a5f
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-03-13 01:56:01 +0530

    Time: 2106 ms (24.35%) | Memory: 42 MB (89.35%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,7 @@
+class Solution{
+    public boolean checkPerfectNumber(int num){
+        int sum=0;
+        for(int i=1;i<num;i++) sum += (num%i==0)?i:0;
+        return (sum==num)?true:false;
+    }
+}
