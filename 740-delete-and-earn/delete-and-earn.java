bbee865ed67d277fdd81a631ee215802eddb891a
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-03 22:37:50 +0530

    Time: 2 ms (99.87%) | Memory: 47 MB (52.01%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,19 +1,23 @@
 class Solution {
-    private int solve(int i, int[]arr,int []dp){
-        if(i==0) return 0;
-        if(i==1) return arr[1];
-        if(dp[i]!=-1) return dp[i];
-        int take=arr[i]+solve(i-2,arr,dp);
-        int skip=solve(i-1,arr,dp);
-        return dp[i]=Math.max(take,skip);
-    }
+    // private int solve(int i, int[]arr,int []dp){
+    //     if(i==0) return 0;
+    //     if(i==1) return arr[1];
+    //     if(dp[i]!=-1) return dp[i];
+    //     int take=arr[i]+solve(i-2,arr,dp);
+    //     int skip=solve(i-1,arr,dp);
+    //     return dp[i]=Math.max(take,skip);
+    // }
     public int deleteAndEarn(int[] nums) {
         int maxVal=0;
         for(int num:nums) maxVal=Math.max(maxVal,num);
         int [] arr=new int[maxVal+1];
         for(int num:nums) arr[num]+=num;
         int []dp=new int[maxVal+1];
-        Arrays.fill(dp,-1);
-        return solve(maxVal,arr,dp);
+        dp[0] = arr[0]; dp[1] = arr[1];
+        for(int i=2;i<=maxVal;i++){
+            dp[i]=Math.max(dp[i-1],arr[i]+dp[i-2]);
+        }
+        return dp[maxVal];
+        // return solve(maxVal,arr,dp);
     }
 }
