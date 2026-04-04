6d4731951359d782cadc0f41e8f1d334d2d89b18
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-05 00:28:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (98.40%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        // int m=grid.length;
+        // int n=grid[0].length;
+        // int[][]dp=new int[m][n];
+        // for(int i=0;i<m;i++) {
+        //     if(grid[i][0]==1) break;
+        //     dp[i][0]=1;
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(grid[0][i]==1) break;
+        //     dp[0][i]=1;
+        // }
+        // for(int i=1;i<m;i++){
+        //     for(int j=1;j<n;j++){
+        //         if(grid[i][j]==1) continue;
+        //         dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+
+        if(grid[0][0] == 1) return 0;
+        int m=grid.length;
+        int n=grid[0].length;
+        int[] dp = new int[n];
+        for(int i=0;i<n;i++){
+            if(grid[0][i]==1) break;
+            dp[i]=1;
+        }
+        for (int i = 1; i < m; i++) {
+            if (grid[i][0] == 1) dp[0] = 0;
+            for (int j = 1; j < n; j++){
+                if (grid[i][j] == 1) dp[j] = 0;
+                else dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
