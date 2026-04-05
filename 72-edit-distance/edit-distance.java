4826dd38d9cfb2e55ba3f83921d69224ca8b928a
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-05 22:02:08 +0530

    Time: 4 ms (78.36%) | Memory: 44.3 MB (99.43%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -11,16 +11,19 @@ public class Solution {
     // }
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][]dp=new int[m+1][n+1];
-        for (int i = 0; i <= m; i++) dp[i][0] = i;
-        for (int j = 0; j <= n; j++) dp[0][j] = j;
+        int[]dp=new int[n+1];
+        for (int j = 0; j <= n; j++) dp[j] = j;
         for(int i=1;i<=m;i++){
+            int prev = dp[0];
+            dp[0] = i; 
             for(int j=1;j<=n;j++){
-                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                int temp=dp[j];
+                if(word1.charAt(i-1)==word2.charAt(j-1)) dp[j]=prev;
+                else dp[j]=1+Math.min(prev,Math.min(dp[j],dp[j-1]));
+                prev=temp;
             }
         }
-        return dp[m][n];
+        return dp[n];
         
         
         // int[][] dp = new int[m + 1][n + 1];
