79d5527fb3b7ed9bf57b7da205a57ce09b05abaf
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-05 23:07:36 +0530

    Time: 3 ms (80.42%) | Memory: 43.5 MB (79.82%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,35 +1,35 @@
 class Solution {
-    private boolean canBreak(String s, Set<String> wordSet, int start, int[]dp) {
-        if (start == s.length()) return true;
-        if(dp[start]!=0) return dp[start]==1;
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (wordSet.contains(s.substring(start, end))) {
-                if (canBreak(s, wordSet, end,dp)) {
-                    dp[start]=1;
-                    return true;
+    // private boolean canBreak(String s, Set<String> wordSet, int start, int[]dp) {
+    //     if (start == s.length()) return true;
+    //     if(dp[start]!=0) return dp[start]==1;
+    //     for (int end = start + 1; end <= s.length(); end++) {
+    //         if (wordSet.contains(s.substring(start, end))) {
+    //             if (canBreak(s, wordSet, end,dp)) {
+    //                 dp[start]=1;
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     dp[start]=-1;
+    //     return false;
+    // }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // Set<String> wordSet = new HashSet<>(wordDict);
+        // int[] dp=new int[s.length()+1];
+        // return canBreak(s, wordSet, 0, dp);
+
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (String w : wordDict) {
+                int start = i - w.length();
+                if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        dp[start]=-1;
-        return false;
-    }
-    public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordSet = new HashSet<>(wordDict);
-        int[] dp=new int[s.length()+1];
-        return canBreak(s, wordSet, 0, dp);
-
-
-        // boolean[] dp = new boolean[s.length() + 1];
-        // dp[0] = true;
-        // for (int i = 1; i <= s.length(); i++) {
-        //     for (String w : wordDict) {
-        //         int start = i - w.length();
-        //         if (start >= 0 && dp[start] && s.substring(start, i).equals(w)) {
-        //             dp[i] = true;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return dp[s.length()];        
+        return dp[s.length()];        
     }
 }
