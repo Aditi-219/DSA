6ba045eb0776f346bcf8c0b0e4f0d312bfd997f2
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-05 22:36:04 +0530

    Time: 1 ms (85.65%) | Memory: 43 MB (72.37%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,21 +1,33 @@
 public class Solution {
-    private int decode(String s, int i,int[]dp){
-        if (i == s.length()) return 1;
-        if (s.charAt(i) == '0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ways=decode(s,i+1,dp);
-        if (i + 1 < s.length()) {
-            int twoDigit = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
+    // private int decode(String s, int i,int[]dp){
+    //     if (i == s.length()) return 1;
+    //     if (s.charAt(i) == '0') return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int ways=decode(s,i+1,dp);
+    //     if (i + 1 < s.length()) {
+    //         int twoDigit = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
+    //         if (twoDigit >= 10 && twoDigit <= 26) {
+    //             ways += decode(s, i + 2,dp);
+    //         }
+    //     }
+    //     return dp[i]=ways;
+    // }
+    public int numDecodings(String s) {
+        int[]dp=new int[s.length()+1];
+        dp[0]=1;
+        dp[1]=s.charAt(0) != '0' ? 1 : 0;
+        for(int i=2;i<=s.length();i++){
+            if (s.charAt(i - 1) != '0')dp[i]+=dp[i-1];
+            int twoDigit = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
             if (twoDigit >= 10 && twoDigit <= 26) {
-                ways += decode(s, i + 2,dp);
+                dp[i] += dp[i-2];
             }
         }
-        return dp[i]=ways;
-    }
-    public int numDecodings(String s) {
-        int[]dp=new int[s.length()];
-        Arrays.fill(dp,-1);
-        return decode(s,0,dp);
+        return dp[s.length()];
+
+
+        // Arrays.fill(dp,-1);
+        // return decode(s,0,dp);
 
         // if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
         // int n = s.length();
