da4627a035a316709cb545ee42ace97ab071c310
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-05 22:42:02 +0530

    Time: 1 ms (85.65%) | Memory: 43 MB (72.37%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -14,33 +14,33 @@ public class Solution {
     // }
     public int numDecodings(String s) {
         int[]dp=new int[s.length()+1];
-        dp[0]=1;
-        dp[1]=s.charAt(0) != '0' ? 1 : 0;
+        int a=1;
+        int b =s.charAt(0) != '0' ? 1 : 0;
         for(int i=2;i<=s.length();i++){
-            if (s.charAt(i - 1) != '0')dp[i]+=dp[i-1];
+            int curr=0;
+            if (s.charAt(i - 1) != '0') curr+=b;
             int twoDigit = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
             if (twoDigit >= 10 && twoDigit <= 26) {
-                dp[i] += dp[i-2];
+                curr += a;
             }
+            a=b;
+            b=curr;
         }
-        return dp[s.length()];
-
-
-        // Arrays.fill(dp,-1);
-        // return decode(s,0,dp);
-
-        // if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
-        // int n = s.length();
-        // int[] dp = new int[n + 1];
-        // dp[0] = 1;        
-        // for (int i = 1; i <= n; i++) {
-        //     if (s.charAt(i - 1) != '0') {
-        //         dp[i] += dp[i - 1];
-        //     }
-        //     if (i > 1 && (s.charAt(i - 2) == '1' || ( s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6'))) {
-        //         dp[i] += dp[i - 2];
+        return b;
+        
+        
+        // int[]dp=new int[s.length()+1];
+        // dp[0]=1;
+        // dp[1]=s.charAt(0) != '0' ? 1 : 0;
+        // for(int i=2;i<=s.length();i++){
+        //     if (s.charAt(i - 1) != '0')dp[i]+=dp[i-1];
+        //     int twoDigit = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+        //     if (twoDigit >= 10 && twoDigit <= 26) {
+        //         dp[i] += dp[i-2];
         //     }
         // }
-        // return dp[n];
+        // return dp[s.length()];
+
+
     }
 }
