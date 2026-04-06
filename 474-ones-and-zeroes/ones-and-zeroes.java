508f4519fe8b1b477ebec9bd43999ace33eb7444
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-07 02:15:54 +0530

    Time: 72 ms (22.69%) | Memory: 99 MB (15.82%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        // return solve(strs, 0, m, n);
+
+        int len = strs.length;
+        int[][][] dp = new int[len][m + 1][n + 1];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j <= m; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        return solve(strs, 0, m, n, dp);
+    }
+    private int solve(String[] strs, int i, int m, int n, int[][][] dp) {
+        if (i == strs.length) return 0;
+        if (dp[i][m][n] != -1) return dp[i][m][n];
+        int zeros = countZeros(strs[i]);
+        int ones = strs[i].length() - zeros;
+        int notTake = solve(strs, i + 1, m, n, dp);
+        int take = 0;
+        if (zeros <= m && ones <= n) {
+            take = 1 + solve(strs, i + 1, m - zeros, n - ones, dp);
+        }
+        return dp[i][m][n] = Math.max(take, notTake);
+    }
+
+    private int solve(String[] strs, int i, int m, int n) {
+        if (i == strs.length) return 0;
+        int zeros = countZeros(strs[i]);
+        int ones = strs[i].length() - zeros;
+        int notTake = solve(strs, i + 1, m, n);
+        int take=0;
+        if (zeros <= m && ones <= n) take = 1 + solve(strs, i + 1, m - zeros, n - ones);
+        return Math.max(take, notTake);
+    }
+    private int countZeros(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '0') count++;
+        }
+        return count;
+    }
+}
