fef9ea6f0dab7f4d74edcd90fc85d466ad7a0a11
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-07 00:16:27 +0530

    Time: 33 ms (68.87%) | Memory: 43.9 MB (85.34%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -14,22 +14,22 @@ class Solution {
     //     return take||notTake;
     // }
     public boolean canPartition(int[] nums) {
-        int totalSum=0;
-        for(int num:nums) totalSum+=num;
-        if (totalSum % 2 != 0) return false;
-        int target=totalSum/2;
-        boolean[][]dp=new boolean[nums.length][target+1];
-        dp[0][0] = true;
-        if (nums[0] <= target) dp[0][nums[0]] = true;
-        for(int i=1;i<nums.length;i++){
-            for(int j=0;j<=target;j++){
-                if(target==0) dp[i][j]=true;
-                boolean take=(nums[i] <= j) ? dp[i - 1][j - nums[i]] : false;
-                boolean notTake=dp[i-1][j];
-                dp[i][j]=take||notTake;
-            }
-        }        
-        return dp[nums.length-1][target];
+        // int totalSum=0;
+        // for(int num:nums) totalSum+=num;
+        // if (totalSum % 2 != 0) return false;
+        // int target=totalSum/2;
+        // boolean[][]dp=new boolean[nums.length][target+1];
+        // dp[0][0] = true;
+        // if (nums[0] <= target) dp[0][nums[0]] = true;
+        // for(int i=1;i<nums.length;i++){
+        //     for(int j=0;j<=target;j++){
+        //         if(target==0) dp[i][j]=true;
+        //         boolean take=(nums[i] <= j) ? dp[i - 1][j - nums[i]] : false;
+        //         boolean notTake=dp[i-1][j];
+        //         dp[i][j]=take||notTake;
+        //     }
+        // }        
+        // return dp[nums.length-1][target];
         
         // int totalSum=0;
         // for(int num:nums) totalSum+=num;
@@ -39,15 +39,17 @@ class Solution {
         // for (int[] row : dp) Arrays.fill(row, -1);
         // return canPartition(nums,0,target,dp);
         
-        // int totalSum = 0;
-        // for (int num : nums) totalSum += num;
-        // if (totalSum % 2 != 0) return false;
-        // int sum = totalSum / 2;
-        // boolean[] dp = new boolean[sum + 1];
-        // dp[0] = true;
-        // for (int num : nums)
-        //     for (int s = sum; s >= num; s--)
-        //         dp[s] = dp[s] || dp[s - num];
-        // return dp[sum];
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+        if (totalSum % 2 != 0) return false;
+        int target = totalSum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for(int i=0;i<nums.length;i++){
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j]=dp[j - nums[i]]||dp[j];
+            }
+        }  
+        return dp[target];
     }
 }
