97af1c864147a900b0acf9dd51e21efc93ab0ae4
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-04-25 12:43:53 +0530

    Time: 6 ms (86.05%) | Memory: 46.6 MB (38.82%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,31 +1,31 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
-        for (int[] f : flights) {
-            graph.get(f[0]).add(new int[]{f[1], f[2]});
+        List<List<int[]>> graph =new ArrayList<>();
+        for(int i=0;i<n;i++) graph.add(new ArrayList<>());
+        for(int [] flight:flights){
+            graph.get(flight[0]).add(new int[]{flight[1],flight[2]});
         }
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, src, 0}); //stopsTaken, node, costSoFar
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int stops = curr[0];
-            int node = curr[1];
-            int cost = curr[2];
-            if (stops > k) continue;
-            for (int[] edge : graph.get(node)) {
-                int nextNode = edge[0];
-                int price = edge[1];
-                int newCost = cost + price;
-                if (newCost < dist[nextNode]) {
-                    dist[nextNode] = newCost;
-                    q.offer(new int[]{stops + 1, nextNode, newCost});
+        int dist[]=new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
+        Queue<int[]> queue=new LinkedList<>();
+        queue.add(new int[]{src,0,0});
+        while(!queue.isEmpty()){
+            int arr[]=queue.poll();
+            int node=arr[0];
+            int cost=arr[1];
+            int stop=arr[2];
+            if(stop>k) continue;
+            for(int[] flight:graph.get(node)){
+                int nextNode=flight[0];
+                int nextCost=flight[1];
+                int newCost=cost+nextCost;
+                if(newCost<dist[nextNode]){
+                    dist[nextNode]=newCost;
+                    queue.add(new int[]{nextNode,newCost,stop+1});
                 }
             }
         }
-        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+        return dist[dst]==Integer.MAX_VALUE? -1 : dist[dst];
     }
 }
