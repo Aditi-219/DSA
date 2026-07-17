720891bfeae7798dedf90656e578316616bede64
Aditi-219 <152678581+Aditi-219@users.noreply.github.com>
2026-07-17 11:34:11 +0530

    Time: 349 ms (70.10%) | Memory: 174.4 MB (53.66%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -4,7 +4,7 @@ class Solution {
         HashMap<Integer, Integer>[] dp = new HashMap[n];
         for (int i = 0; i < n; i++) dp[i] = new HashMap<>();
         int maxLen = 2;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 int diff = nums[i] - nums[j];
                 int prev = dp[j].getOrDefault(diff, 1);
